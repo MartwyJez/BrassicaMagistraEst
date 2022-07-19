88ed683408744f79a2429c13071b1b6ac86a07af
NacixWosinska <natalia.wosinska@capgemini.com>
2022-07-19 12:03:32 +0200

    csv logger done

--- app/src/main/java/ib/edu/heart/HeartBeatActivity.kt
+++ app/src/main/java/ib/edu/heart/HeartBeatActivity.kt
@@ -11,29 +11,25 @@ import android.widget.EditText
 import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import com.google.gson.Gson
-import com.google.gson.JsonArray
 import com.polar.androidblesdk.R
 import com.polar.sdk.api.PolarBleApi
 import com.polar.sdk.api.PolarBleApiCallback
 import com.polar.sdk.api.PolarBleApiDefaultImpl
 import com.polar.sdk.api.model.PolarHrData
 import java.util.*
-import kotlin.collections.ArrayList
-import com.google.gson.reflect.TypeToken as TypeToken
 
 class HeartBeatActivity : AppCompatActivity() {
-
+    private var mCsvLogger: CsvLogger? = null
     private var seconds = 0
     private var running = false
     private var intervals = 0
     private var heartBeat = 0
     private val waiting = "Oczekiwanie na rozpoczęcie badania ..."
-
+    private var isLogSaved = false
     private var sensorRecord = arrayListOf<Int>()
 
     private var userRecord = arrayListOf<String>()
-
+    private val LOG_TAG: String = HeartBeatActivity::class.java.simpleName
     private lateinit var textView: TextView
     private lateinit var userTextView: TextView
     private lateinit var buttonStart: Button
@@ -65,17 +61,16 @@ class HeartBeatActivity : AppCompatActivity() {
         userText = findViewById(R.id.userHeartBeat)
         submit = findViewById(R.id.submitButton)
         userTextView = findViewById(R.id.userTextView)
-        //przekazane interwały
 
+        mCsvLogger = CsvLogger()
         val data:String = intent.getStringExtra("arrayIntervals").toString()
-
+        mCsvLogger!!.checkRuntimeWriteExternalStoragePermission(this, this)
         intervalsTable = ArrayListObjectParser.fromJson(data) as ArrayList<CustomListElement>
 
-        println("HOHOHO" + intervalsTable.toString())
 
         disableEditText(userText)
 
-
+        mCsvLogger!!.appendHeader("Lp., Interval [ms], User expectations, Real EKG score")
 
         api.setApiLogger { s: String -> Log.d(HeartBeatActivity.API_LOGGER_TAG, s) }
         api.setApiCallback(object : PolarBleApiCallback() {
@@ -97,21 +92,35 @@ class HeartBeatActivity : AppCompatActivity() {
 
 
         buttonStart.setOnClickListener {
+            isLogSaved = false
             running = true
         }
+        var i = 0
 
         submit.setOnClickListener {
+
             userRecord.add(userText.text.toString())
             println(userRecord.toString())
+            mCsvLogger!!.appendLine(
+                String.format(
+                    Locale.getDefault(),
+                    "%s,%s,%s,%s", (i + 1).toString(), intervalsTable.get(i).duration.toString(), userRecord[i].toString(),
+                    heartBeat.toString()
+                )
+            )
+            i++
+            if(intervals == intervalsTable.size) {
+                if (!isLogSaved) {
+                    mCsvLogger!!.finishSavingLogs(this@HeartBeatActivity, LOG_TAG)
+                    isLogSaved = true
+                }
+            }
             disableEditText(userText)
             userText.text.clear()
         }
 
+        }
 
-
-
-
-    }
 
     private fun disableEditText(editText: EditText) {
         userTextView.text = ""
@@ -148,18 +157,20 @@ class HeartBeatActivity : AppCompatActivity() {
                 }
                 handler.postDelayed({ this.run() }, 1000)
 
-                    println(intervals)
+
                     if (seconds == intervalsTable.get(intervals).duration) {
                         running = false
                         seconds = 0
                         intervals++
                         sensorRecord.add(heartBeat)
+                        println(sensorRecord.toString())
                         heartBeat = 0
                         textView.text = "Interwał zakończony. Wpisz ilość uderzeń serca i wystartuj kolejny interwał"
                         enableEditText(userText)
 
                     }
                 }else{
+
                     println("PRZEJDZ DO OKNA WYNIKOW")
                 }
 
